8b29572e2c11894356c0c663db64bde9974271be
Maicon <mviconoliveirv@gmail.com>
2024-10-08 21:51:41 -0300

    Add files via upload

---
+++ ListTest.java
@@ -0,0 +1,14 @@
+// Fig. 12.24: ListTest.java
+// Selecting colors from a JList.
+import javax.swing.*;
+
+public class ListTest 
+{// declaração da classe listtest
+   public static void main(String[] args) // declaração do metodo main
+   { 
+      ListFrame listFrame = new ListFrame(); // create ListFrame// declaração de
+      listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      listFrame.setSize(350, 150); 
+      listFrame.setVisible(true); 
+   } 
+} // end class ListTest
