515844e527cc3668288d62750ebc316246933d20
Maicon <mviconoliveirv@gmail.com>
2024-10-09 21:01:21 -0300

    Add files via upload

---
+++ RadioButtonTest.java
@@ -0,0 +1,15 @@
+// Fig. 12.20: RadioButtonTest.java
+// Testing RadioButtonFrame.
+import javax.swing.*; //
+
+public class RadioButtonTest  
+{
+   public static void main(String[] args)
+   {
+      RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+      radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      radioButtonFrame.setSize(300, 100); 
+      radioButtonFrame.setVisible(true); 
+   } 
+} // end class RadioButtonTest 
+
