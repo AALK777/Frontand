cd69137e923f2f25f382a36492b525cb1b7c8311
Maicon <mviconoliveirv@gmail.com>
2024-10-10 21:55:29 -0300

    Add files via upload

---
+++ MenuTest.java
@@ -0,0 +1,18 @@
+// Fig. 19.6: MenuTest.java
+// Testing MenuFrame.
+import javax.swing.*;
+
+public class MenuTest
+{
+   public static void main(String[] args)
+   { 
+      MenuFrame menuFrame = new MenuFrame(); 
+      menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      menuFrame.setSize(500, 200); 
+      menuFrame.setVisible(true);
+// menuFrame
+
+   } 
+
+} 
+
